4cb0562dcb94db8f9767e0b418ad97f790ba0f5f
lookup57lookup57-bit <lookup57lookup57@gmail.com>
2026-06-23 11:15:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
+echo '#!/bin/bash
 pip install -r requirements.txt
-playwright install chromium
+playwright install chromium' > build.sh
+
+chmod +x build.sh
+git add build.sh
+git commit -m "Correct build.sh"
+git push
